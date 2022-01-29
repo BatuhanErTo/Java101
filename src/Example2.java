7c27d77ce29ae92ac000bb2b981ee9bf00463b19
Batuhan Erol <bataer57@gmail.com>
2022-01-29 23:33:52 +0300

    Second example has finished and run succesfully

---
+++ src/Example2.java
@@ -0,0 +1,14 @@
+import  java.util.Scanner;
+public class Example2 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double fee = inp.nextInt();
+        int vat = 18;
+        if(fee>1000){
+            vat = 8;
+        }
+        double result1 = fee % vat;
+        double result2 = fee + result1;
+        System.out.println(result1+" "+result2);
+    }
+}
