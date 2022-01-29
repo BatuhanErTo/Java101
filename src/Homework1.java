7deadd41b723f2c20af409abd1810f9563a0f245
Batuhan Erol <bataer57@gmail.com>
2022-01-30 00:50:29 +0300

    Homework1 has succefully finished and checked

---
+++ src/Homework1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Homework1 {
+    public static void main(String[] args) {
+        double height, weight;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Please enter your height and weight (Height must be meter)");
+        height = inp.nextDouble();
+        weight = inp.nextDouble();
+        double bodyMassIndex = weight / (height * height);
+        System.out.println("Your boddy mass index is " + bodyMassIndex);
+    }
+}
