9df05a759c5bfd907d2ea00d33cc96f7d51c8705
Batuhan Erol <bataer57@gmail.com>
2022-01-30 00:09:47 +0300

    Example5 has finished and run succesfully

---
+++ src/Example5.java
@@ -0,0 +1,15 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+public class Example5 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int entranceFee = 10,minFee = 20;
+        double per_km = 2.20;
+        double km = inp.nextDouble();
+        double price = entranceFee + km * per_km;
+        if(price<minFee){
+            price = minFee;
+        }
+        System.out.println("Prace is that you must pay to taxi driver is "+price);
+    }
+}
