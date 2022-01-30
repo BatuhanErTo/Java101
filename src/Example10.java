c38a4530bcd084fdef18bcd3d74b45faf8ce62db
Batuhan Erol <bataer57@gmail.com>
2022-01-30 15:42:58 +0300

    Example10 has succesfully finished

---
+++ src/Example10.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Example10 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter the heat");
+        int heat = inp.nextInt();
+        if(heat<5){
+            System.out.println("Snowboarding");
+        }else if(heat<25){
+            if(heat<15){
+                System.out.println("Theatre");
+            }else{
+                System.out.println("Picnic");
+            }
+        }else{
+            System.out.println("Swimming");
+        }
+    }
+}
