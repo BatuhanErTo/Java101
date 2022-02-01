8843678e09a685b1481569ae2953efb57d00b1eb
Batuhan Erol <bataer57@gmail.com>
2022-02-01 17:03:50 +0300

    Example17 has succesfully finished

---
+++ src/Example17.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Example17  {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int base,exponent,result = 1;
+        System.out.print("Base --> ");
+        base = inp.nextInt();
+        System.out.print("Exponent--> ");
+        exponent = inp.nextInt();
+        for (int i=1;i<=exponent;i++) {
+            result *= base;
+        }
+        System.out.println(result);
+    }
+}
