574b2807f2ef7b4ee606d9a19ade41968350c82f
Batuhan Erol <bataer57@gmail.com>
2022-02-01 16:09:33 +0300

    Example14 has succesfully finished

---
+++ src/Example14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Example14 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int number,sum=0;
+        do {
+            number = inp.nextInt();
+            if((number%2)==0){
+                if((number%4)==0){
+                    sum += number;
+                }
+            }
+        }while((number%2)!=1);
+        System.out.println(sum);
+    }
+}
+
