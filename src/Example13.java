5588fe61d05a5422ed6002199a6bf43f1ad02bae
Batuhan Erol <bataer57@gmail.com>
2022-02-01 15:55:23 +0300

    Example13 has succesfully finished

--- src/Example13.java
+++ src/Example13.java
@@ -4,10 +4,13 @@ public class Example13 {
         Scanner inp = new Scanner(System.in);
         System.out.print("Enter a number --> ");
         int number = inp.nextInt();
-        for(int i = 0;i<=number;i++){
+        double tracker = 0,sum = 0;
+        for(int i = 1;i<=number;i++){
             if((i%3)==0&&(i%4)==0){
-                System.out.println(i);
+                sum += i;
+                tracker++;
             }
         }
+        System.out.println(sum/tracker);
     }
 }
