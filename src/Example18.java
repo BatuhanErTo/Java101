4e7c39509966c74baec09cb9aecb68cb5dfb3f1f
Batuhan Erol <bataer57@gmail.com>
2022-02-02 04:41:53 +0300

    Example18 has succesfully finished

---
+++ src/Example18.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Example18 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter a number,please --> ");
+        int number = inp.nextInt();
+        int tempnumber = number;
+        int tracker=0,sum=0;
+        while(tempnumber != 0){
+            tempnumber=tempnumber /10;
+            tracker++;
+        }
+        for (int i=1;i<=tracker;i++) {
+            sum += number%10;
+            number = number / 10;
+        }
+        System.out.println(sum);
+    }
+}
