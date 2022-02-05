5c95a5ba572bf2f147d0b88648a8dedf40d6c37c
Batuhan Erol <bataer57@gmail.com>
2022-02-05 17:00:38 +0300

    Homework19 has succesfully finished

---
+++ src/Java101/Homework10.java
@@ -0,0 +1,19 @@
+package Java101;
+import java.util.Scanner;
+public class Homework10 {
+    public static void main(String[] args) {
+        int n1=0,n2=1;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("How many Fibonacci numbers will be calculated: ");
+        int n = inp.nextInt();
+        int i = 1;
+        while(i<=n){
+            System.out.print(n1+"-");
+            int sum = n1+n2;
+            n1 = n2;
+            n2 = sum;
+            i++;
+        }
+
+    }
+}
